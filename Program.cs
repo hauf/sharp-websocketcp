bcfaa9ed1645d2a190a9f7899aa8bd1ba68b2c57
hauf <haowin99@hotmail.com>
2021-11-05 16:17:31 +0800

    项目初始化

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using CliFx;
+using Microsoft.Extensions.DependencyInjection;
+using sharp_websocketcp.Commands;
+using sharp_websocketcp.Handler;
+using System;
+using System.Threading.Tasks;
+using WebSocketSharp.Server;
+
+namespace sharp_websocketcp
+{
+
+    class Program
+    {
+        public static async Task<int> Main() =>
+           await new CliApplicationBuilder()
+               .AddCommandsFromThisAssembly()
+               .UseTypeActivator(GetServiceProvider().GetRequiredService)
+               .Build()
+               .RunAsync();
+
+        private static IServiceProvider GetServiceProvider()
+        {
+            var services = new ServiceCollection();
+
+            services.AddTransient<ConnectionCommand>();
+
+            return services.BuildServiceProvider();
+        }
+
+    }
+}
